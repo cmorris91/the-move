afc26ddf38c5f48f17414247c3205f1efee7d5a0
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-04-15 10:41:19 -0700

    boiler plate added

---
+++ client/build/precache-manifest.1e8ad20276eb465405727ec09a1699dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac5517cac92cc801954e6bb6e1170ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea3f0f936b2111f4a79",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea3f0f936b2111f4a79",
+    "url": "/static/js/main.3cb22cd5.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
