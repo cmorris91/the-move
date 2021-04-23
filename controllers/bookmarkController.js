5f5576997fcdfa42aeec26d2d13179b77ad4ea11
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-04-22 17:49:41 -0700

    bookmarks now function, next up is getting them to display all events

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,16 +1,37 @@
 const db = require("../models");
 
 module.exports = {
+  findAll: function(req, res) {
+    db.Bookmark
+      .find(req.query)
+      .populate("event","User")
+      .then(dbModel => {
+        res.json(dbModel)})
+      .catch(err => res.status(422).json(err));
+  },
+  findAllById: function(req,res){
+    db.Bookmark
+    .find({user:req.body.name})
+    .populate("Event", "User")
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err));
+  },
   findById: function(req, res) {
     db.Bookmark
       .find({user:req.body.name})
       .populate("Event", "User")
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => 
+        {if(dbModel[0]){
+          res.json(true)
+      }else if(!dbModel[0]){
+        res.json(false)
+      }
+    })
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.Bookmark
-      .create(req.body)
+      .create({events:req.body.events, user:req.body.name})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
@@ -18,7 +39,7 @@ module.exports = {
     const info = req.body
     db.Bookmark
       .update({user:info.name},{$push:{events: info.events}}) 
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => console.log(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
