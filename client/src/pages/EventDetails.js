c65ace6560430eb175b876a5dc91c3183fedc9d6
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-04-23 13:54:08 -0700

    fixed various bugs with code

--- client/src/pages/EventDetails.js
+++ client/src/pages/EventDetails.js
@@ -3,13 +3,10 @@ import API from "../utils/API";
 import {Link} from "react-router-dom"
 import { Grid, Image, Icon, Button, Container } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css'
-<<<<<<< HEAD
-import Images2 from"../components/Images2"
-import Afeedback from"../components/Afeedback"
-=======
 import DeleteButton from "../../src/components/Delete"
+import Afeedback from "../components/Afeedback"
+import Images2 from "../components/Images2";
 
->>>>>>> ce08e49b268629b8e1389990b1ef676331ff0eac
 
 function EventDetail () {
         //first state is for the event, the second is for bookmark
@@ -65,20 +62,6 @@ function refresh(){
     }
 
 
-<<<<<<< HEAD
-console.log(saveBookmark.images)
-=======
-    function componentDidMount(){
-        const user =localStorage.getItem("user")
-        let info = id + "/" + user
-        const info2 = info.split("/")
-        API.getBookmark(info2)
-          .then(res => {
-            console.log(res)
-           setSingleEvent({ isBookmark: res.data })})
-          .catch (err => console.log(err));
-      }
-
       function handleDelete() {
         API.deleteEvent(id)
         .then(res => {
@@ -88,7 +71,6 @@ console.log(saveBookmark.images)
         .catch(err => console.log(err))
       }
 
->>>>>>> ce08e49b268629b8e1389990b1ef676331ff0eac
     //renders the event data
     return (
         <Container textAlign="center">
@@ -109,21 +91,13 @@ console.log(saveBookmark.images)
                     <br/>
             {singleEvent.description}
                     <br/>
-<<<<<<< HEAD
-            {singleEvent.date}
-=======
-            {new Date(singleEvent.event.date).toDateString()}
->>>>>>> ce08e49b268629b8e1389990b1ef676331ff0eac
+            {new Date(singleEvent.date).toDateString()}
                     <br/>
             {singleEvent.address}
                     <br/>
             {singleEvent.city}, {singleEvent.state} {singleEvent.zipcode}
                     <br/>
-<<<<<<< HEAD
-            {singleEvent.date_created}
-=======
-            {new Date(singleEvent.event.date_created).toDateString()}
->>>>>>> ce08e49b268629b8e1389990b1ef676331ff0eac
+            {new Date(singleEvent.date_created).toDateString()}
                     <br/>
             <em>rating: {singleEvent.averageRating}</em></p>
             <br/>
@@ -145,7 +119,7 @@ console.log(saveBookmark.images)
                 </Link>
                 <Images2 images={singleEvent.images} />
             </Grid.Column>
-            {singleEvent.event.user_id === uid ? (
+            {singleEvent.user_id === uid ? (
                 <DeleteButton 
                 onClick={handleDelete}
                 />    
