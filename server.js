5e2ac6e6601143d7793495546273d6a20976f229
Chelsey Morris <chelseymorris91@yahoo.com>
2021-04-25 16:06:12 -0700

    delpoinggggg

--- server.js
+++ server.js
@@ -51,7 +51,14 @@ app.use(routes);
 
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/the-move");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/the-move",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 // Start the API server
 app.listen(PORT, function() {
